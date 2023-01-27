0f1bf18815a95cc8902921e965e17a970381e975
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-01-27 10:25:01 +0900

    2개의 배열 필요 -> 2차원 배열 & 절댓값 함수 abs

--- 0x03배열/1919.cpp
+++ 0x03배열/1919.cpp
@@ -7,28 +7,17 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a[27] = {0,};
-    int b[27] = {0,};
+    int arr[2][27] = {0,};
 
     string x, y;
     cin >> x >> y;
 
-    for (char c: x) a[c-'a']++;
-    for (char c: y) b[c-'a']++;
+    for (char c: x) arr[0][c-'a']++;
+    for (char c: y) arr[1][c-'a']++;
 
     int ans = 0;
     for (int i=0; i<27; i++){
-        if (a[i] > b[i]) {
-           while (a[i] > b[i]) {
-            a[i]--;
-            ans++;
-           }
-        } else if (a[i] < b[i]) {
-            while (a[i] < b[i]) {
-                b[i]--;
-                ans++;
-            }
-        } 
+        ans += abs(arr[0][i]-arr[1][i]);
     }
     cout << ans;
 
