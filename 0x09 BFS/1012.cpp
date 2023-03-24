400d8fdafcaa752739a2fa6ffdb6fccae9ce784d
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-03-24 22:22:09 +0900

    Flood Fill (코드 정리)

--- 0x09 BFS/1012.cpp
+++ 0x09 BFS/1012.cpp
@@ -8,6 +8,34 @@ using namespace std;
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
+int m, n, k;
+int ans = 0;
+int board[52][52];
+bool vis[52][52];
+
+void bfs(int x, int y) {
+    queue<pair<int, int>> Q;
+    Q.push(make_pair(x, y));
+    vis[x][y] = true;
+
+    while (!Q.empty())
+    {
+        auto cur = Q.front();
+        Q.pop();
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+                continue;
+            if (vis[nx][ny] || board[nx][ny] != 1)
+                continue;
+            vis[nx][ny] = true;
+            Q.push(make_pair(nx, ny));
+        }
+    }
+}
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -15,16 +43,13 @@ int main(){
     int T;
     cin >> T;
     while (T--) {
-        int m, n, k;
-        int ans = 0;
         cin >> m >> n >> k;
-
-        int board[52][52];
-        bool vis[52][52];
+        
         for (int i=0; i<52; i++) {
             fill(board[i], board[i]+52, 0);
             fill(vis[i], vis[i]+52, false);
         }
+        ans = 0;
 
         while(k--){
             int x, y;
@@ -35,28 +60,7 @@ int main(){
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
                 if (board[i][j] != 1 || vis[i][j]) continue;
-
-                queue<pair<int, int> > Q;
-                Q.push(make_pair(i, j));
-                vis[i][j] = true;
-                int cnt = 0;
-                while (!Q.empty())
-                {
-                    auto cur = Q.front();
-                    Q.pop();
-                    for (int dir = 0; dir < 4; dir++)
-                    {
-                        int nx = cur.X + dx[dir];
-                        int ny = cur.Y + dy[dir];
-                        if (nx < 0 || nx >= m || ny < 0 || ny >= n)
-                            continue;
-                        if (vis[nx][ny] || board[nx][ny] != 1) continue;
-                        vis[nx][ny] = true;
-                        Q.push(make_pair(nx, ny));
-                    }
-                    cnt++;
-                }
-
+                bfs(i, j);
                 ans++;
             }
         }
