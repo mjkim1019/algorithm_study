7561ff5fa3602074ed436536a855156b301854f7
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-04-05 01:40:19 +0900

    input 주어진 오름차순 수열

---
+++ 0x0C 백트래킹/15655.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int ans[10];
+int input[10];
+bool isUsed[10];
+
+void func(int L){
+    if (L == M) {
+        for (int i=0; i<M; i++) cout << input[ans[i]] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int start = 0;
+    if (L != 0) start = ans[L-1];
+    for (int i=start; i < N; i++){
+        if (!isUsed[i]){
+            ans[L] = i;
+            isUsed[i] = true;
+            func(L+1);
+            isUsed[i] = false;
+        }
+    }
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i=0; i<N; i++) cin >> input[i];
+    sort(input, input+N);
+    func(0);
+
+    return 0;
+}
