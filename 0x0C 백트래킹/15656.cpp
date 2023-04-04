6c96ca6f785d21f95a575f1ecd57923fd6aa01d7
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-04-05 01:45:33 +0900

    input 중복 허용 수열

---
+++ 0x0C 백트래킹/15656.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int ans[10];
+int input[10];
+
+void func(int L)
+{
+    if (L == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << input[ans[i]] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        ans[L] = i;
+        func(L + 1);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        cin >> input[i];
+    sort(input, input + N);
+    func(0);
+
+    return 0;
+}
