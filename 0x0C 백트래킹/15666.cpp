4caeae31ad143b2a4c17b8de4a62742e501f2d0b
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-04-06 16:46:54 +0900

    중복 input, 중복 택, 비내림차순, 중복 수열 금지

---
+++ 0x0C 백트래킹/15666.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int input[10];
+int ans[10];
+
+void func(int L, int start)
+{
+    if (L == M)
+    {
+        for (int i = 0; i < M; i++)
+            cout << ans[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int tmp = 0;
+    for (int i = start; i < N; i++)
+    {
+        if (tmp != input[i])
+        {
+            ans[L] = input[i];
+            tmp = input[i];
+            func(L + 1, i);
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        cin >> input[i];
+    sort(input, input + N);
+    func(0, 0);
+    return 0;
+}
