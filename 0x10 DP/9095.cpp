98d0718a14f2d3d537d5463e8d582b692e30cfc2
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-04-12 16:38:51 +0900

    d[1]부터 d[N]까지 미리 다 구해두는 게 효율적!

--- 0x10 DP/9095.cpp
+++ 0x10 DP/9095.cpp
@@ -7,17 +7,19 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    // 초기값 정하기
+    d[1] = 1; d[2] = 2; d[3] = 4;
+    // n이 주어질 때마다 매번 새로 d[1]부터 d[n]까지 구하는 것보다 미리 다 구해두는 게 효율적!
+    for (int i = 4; i < 11; i++)
+    {
+        d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+    }
+
     int TC;
     cin >> TC;
     while (TC--){
         int N;
         cin >> N;
-
-        // 초기값 정하기
-        d[1] = 1; d[2] = 2; d[3]=4;
-        for (int i=4; i<=N; i++){
-            d[i] = d[i-1] + d[i-2] + d[i-3];
-        }
         cout << d[N] << '\n';
     }
 
