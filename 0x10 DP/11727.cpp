79e05dd413563ea3c1654f717010ce823f93723e
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-04-17 00:58:52 +0900

    Create 11727.cpp

---
+++ 0x10 DP/11727.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int d[1002];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    d[1] =1;
+    d[2] =3;
+    for (int i=3; i<=n; i++){
+        d[i] = (d[i-1] + 2*d[i-2]) % 10007;
+    }
+    cout << d[n];
+    
+    return 0;
+}
