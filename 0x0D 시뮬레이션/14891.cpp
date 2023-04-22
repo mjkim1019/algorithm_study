fafed8a7dcacd8594a3f223ccf3621800c4d6a1e
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-04-23 02:32:55 +0900

    방향 변수 따로 배열에 저장 (어차피 4개) & rotate(): 왼쪽으로 리스트 회전

--- 0x0D 시뮬레이션/14891.cpp
+++ 0x0D 시뮬레이션/14891.cpp
@@ -1,83 +1,59 @@
 #include <iostream>
-#include <deque>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
-deque<int> wheel[5]; // 1,2,3,4 사용
 int k; // 회전 횟수
-int stnum;
-int ennum;
-bool visited[5];
+string wheel[4];
 
-int reverseDir(int dir){
-    if (dir > 0)
-        return -1;
-    else
-        return 1;
-}
+void go(int x, int dir){
+    int dirs[4] = {};
+    dirs[x] = dir;
+    // 왼쪽으로 회전을 전파
+    int idx = x;
+    while (idx > 0 && wheel[idx][6] != wheel[idx - 1][2])
+    {
+        dirs[idx - 1] = -dirs[idx];
+        idx--;
+    }
 
-void rotate(int num, int dir){
-    if (dir == 1) { // 시계 방향
-        int value = wheel[num].back();
-        wheel[num].pop_back();
-        wheel[num].push_front(value);
-    } else { // 반시계 방향
-        int value = wheel[num].front();
-        wheel[num].pop_front();
-        wheel[num].push_back(value);
+    // 오른쪽으로 회전을 전파
+    idx = x;
+    while (idx < 3 && wheel[idx][2] != wheel[idx + 1][6])
+    {
+        dirs[idx + 1] = -dirs[idx];
+        idx++;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (dirs[i] == -1)
+            rotate(wheel[i].begin(), wheel[i].begin() + 1, wheel[i].end());
+        else if (dirs[i] == 1)
+            rotate(wheel[i].begin(), wheel[i].begin() + 7, wheel[i].end());
     }
 }
-
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    string input[5];
-    for (int i=1; i<=4; i++)
-        cin >> input[i];
-    for (int i=1; i<=4; i++)
-        for (int j=0; j<8; j++)
-            wheel[i].push_back(input[i][j]-'0');
-    
+    for (int i=0; i<4; i++)
+        cin >> wheel[i];
+
     cin >> k;
     while (k--){
         int num, dir;
         cin >> num >> dir;
         
-        queue<pair<int, int> > Q;
-        fill(visited, visited+5, false);
-        Q.push(make_pair(num, dir));
-        visited[num] = true;
-        while (!Q.empty()){
-            auto cur = Q.front(); Q.pop();
-            int num = cur.first;
-            int dir = cur.second;
-            //cout << '\n' << num << ", " << dir << '\n';
-
-            // 오른쪽 확인
-            if (num < 4 && !visited[num+1] && wheel[num].at(2) != wheel[num + 1].at(6)) {
-                Q.push(make_pair(num + 1, reverseDir(dir)));
-                visited[num+1] = true;
-            }
-            // 왼쪽 확인
-            if (num > 1 && !visited[num-1] && wheel[num].at(6) != wheel[num-1].at(2)) {
-                Q.push(make_pair(num - 1, reverseDir(dir)));
-                visited[num-1] = true;
-            }
-
-            // dir 방향으로 회전
-            rotate(num, dir);
-        }
-
+        go(num-1, dir);
     }
 
     int ans = 0;
-    for (int i=1; i<=4; i++){
-        if (wheel[i].front() == 1) ans += 1<<(i-1);
+    for (int i=0; i<4; i++){
+        if (wheel[i][0] == '1') ans += (1<<i);
     }
     cout << ans;
+   
 
     return 0;
 }
