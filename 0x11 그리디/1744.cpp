f5b3485e389c05b9b591966d9f259988bcd2c1f6
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-05-23 18:22:52 +0900

    음수 양수 나눠서, 오름차순은 greater<>() 사용

--- 0x11 그리디/1744.cpp
+++ 0x11 그리디/1744.cpp
@@ -3,9 +3,21 @@
 #include <algorithm>
 using namespace std;
 
-bool cmp(int &a, int &b)
-{
-    return a > b;
+int sum(vector<int> &v){
+    int ans = 0;
+    if (v.size() < 2)
+    {
+        for (int &i : v) ans += i;
+    }
+    else
+    {
+        for (int i = 1; i < v.size(); i += 2)
+            ans += v[i] * v[i - 1];
+        
+        if (v.size() % 2 == 1)
+            ans += v[v.size() - 1];
+    }
+    return ans;
 }
 
 int main()
@@ -13,47 +25,26 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    int ans = 0;
+
     int N;
     cin >> N;
-    vector<int> plus;
-    vector<int> minus;
+    vector<int> plus, minus;
     for (int i = 0; i < N; i++)
     {
         int tmp;
         cin >> tmp;
-        if (tmp > 0)
-            plus.push_back(tmp);
-        else
-            minus.push_back(tmp);
+        if (tmp == 1) ans += tmp;
+        else if (tmp > 0) plus.push_back(tmp);
+        else minus.push_back(tmp);
     }
-    sort(plus.begin(), plus.end(), cmp);
+    sort(plus.begin(), plus.end(), greater<int>());
     sort(minus.begin(), minus.end());
 
-    int ans = 0;
-    if (plus.size() < 2)
-    {
-        for (int &i : plus) ans += i;
-    }
-    else
-    {
-        for (int i = 1; i < plus.size(); i += 2)
-        {
-            if (plus[i] >= 2 && plus[i - 1] >= 2)
-                ans += plus[i] * plus[i - 1];
-            else
-                ans += plus[i] + plus[i - 1];
-        }
-        if (plus.size() %2 == 1) ans += plus[plus.size()-1];
-    }
-
-    if (minus.size() < 2){
-        for (int &i: minus) ans += i;
-    } else {
-        for (int i=1; i<minus.size(); i+=2) {
-            ans += minus[i] * minus[i-1];
-        }
-        if (minus.size() %2 == 1) ans += minus[minus.size()-1];
-    }
+    
+    ans += sum(plus);
+    ans += sum(minus);
+    
     cout << ans;
 
     return 0;
