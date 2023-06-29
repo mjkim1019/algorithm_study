72c562061e6c544da4e2f7b091f94f3958a28286
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-06-29 23:19:24 +0900

    for문 사용시 복사 안되게 & 사용해주기!

--- 프로그래머스/Lv1/대충 만든 자판.cpp
+++ 프로그래머스/Lv1/대충 만든 자판.cpp
@@ -9,13 +9,13 @@ vector<int> solution(vector<string> keymap, vector<string> targets) {
     
     fill(arr, arr+27, 101);
     
-    for (string key: keymap){
+    for (string &key: keymap){
         for (int i=0; i<key.size(); i++){
             arr[key[i]-'A'] = min(i+1, arr[key[i]-'A']);
         }
     }
     
-    for (string target: targets){
+    for (string &target: targets){
         int res = 0;
         int i;
         for (i=0; i<target.size(); i++){
