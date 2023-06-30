3f862cbbbfc39c974fe155e5aaeb13bcf9460994
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-06-30 19:50:21 +0900

    내림차순 정렬~

---
+++ 복습/2217(2차).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    vector<int> rope(N);
+    for (int i=0; i<N; i++) cin >> rope[i];
+
+    sort(rope.begin(), rope.end(), greater<>());
+    int ans = rope[0];
+    for (int i=1; i<N; i++){
+        ans = max(ans, rope[i]*(i+1));
+    }
+    cout << ans;
+
+    return 0;
+}
