ccbc563864aa00835703dda24f05a301bb64bb68
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-07-02 23:58:48 +0900

    Create 햄버거 만들기.cpp

---
+++ 프로그래머스/Lv1/햄버거 만들기.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int hambuger[4] = {1, 3, 2, 1};
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    
+    stack<int> S;
+    for (int i=0; i<ingredient.size(); i++){
+        if (S.size() <4) {
+            S.push(ingredient[i]);
+            continue;
+        }
+        
+        stack<int> S2;
+        int cnt = 0;
+        while (!S.empty() && cnt < 4){
+            if (S.top() != hambuger[cnt]) break;
+            S2.push(S.top()); S.pop();
+            cnt++;
+        }
+        if (cnt == 4) answer++;
+        else {
+            while (!S2.empty()){
+                S.push(S2.top()); 
+                S.pop();
+            }
+        }
+    }
+    
+    return answer;
+}
