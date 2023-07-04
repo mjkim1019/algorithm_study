b88fbe50da90944425239033ebb90b3c23ddfbad
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-07-04 16:09:12 +0900

    toupper()와 tolower는 대소문자 외 일반 문자 들어오면 그냥 반환해줌 -> 따로 처리 필요없음

--- 프로그래머스/Lv2/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/Lv2/JadenCase 문자열 만들기.cpp
@@ -5,14 +5,11 @@ using namespace std;
 string solution(string s) {
     string answer = "";
     
-    if (s[0] != ' ' && !isdigit(s[0])) s[0] = toupper(s[0]);
+    answer += toupper(s[0]);
     for (int i=1; i<s.size(); i++){
-        if (s[i] == ' ') continue;
-        if (isdigit(s[i])) continue;
-        if (s[i-1] == ' ') s[i] = toupper(s[i]);
-        else s[i] = tolower(s[i]);
+        if (s[i-1] == ' ') answer += toupper(s[i]);
+        else answer += tolower(s[i]);
     }
-    answer = s;
     
     return answer;
 }
