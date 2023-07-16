da6c3be0e15fa4760065c6652380353dc3fcfcef
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-07-16 23:49:42 +0900

    unordered_map -> vector로 바로 변경 가능

--- 0x15 해시/13414.cpp
+++ 0x15 해시/13414.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
-int main(){
+bool cmp(pair<string, int> &a, pair<string, int> &b){
+    return a.second < b.second;
+}
+
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
@@ -12,23 +18,22 @@ int main(){
 
     vector<string> names;
     unordered_map<string, int> table; // 위치 저장
-    for (int i=0; i<L; i++){
+    for (int i = 0; i < L; i++)
+    {
         string str;
         cin >> str;
-        if (table.find(str) != table.end())
-            names[table[str]-1] = "";
-        table[str] = i+1;
-        names.push_back(str);
+        if (table.find(str) != table.end()) // 이미 존재
+            table.erase(str);
+        table[str] = i;
     }
 
-    //cout <<'\n';
-    int cnt = 0;
-    for (string name: names){
-        if (name == "") continue;
-        cout << name <<'\n';
-        cnt++;
-        if(cnt == K) break;
+    vector<pair<string, int>> slist(table.begin(), table.end());
+    sort(table.begin(), table.end(), cmp);
+
+    for (int i=0; i< min((int)slist.size(), K); i++){
+        cout << slist[i].first <<'\n';
     }
+
 
     return 0;
 }
