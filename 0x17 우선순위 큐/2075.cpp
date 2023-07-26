f8832576e8a3f3eb0c5b4c00286475a039f7b60e
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-07-26 16:08:32 +0900

    N번째 큰 수를 찾는다 == 상위 5개만 필요하다 -> 최소힙으로 나머지들 제거해도 ok

--- 0x17 우선순위 큐/2075.cpp
+++ 0x17 우선순위 큐/2075.cpp
@@ -1,24 +1,22 @@
-// Authored by : heheHwang
-// Co-authored by : -
-// http://boj.kr/45e1c40b488544b89ca5d9c966a6cdc0
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-  int N, num;
-  priority_queue<int, vector<int>, greater<>> pq;
-  cin >> N;
-  for (int i = 0; i < N * N; i++) {
-    cin >> num;
-    pq.push(num);
-    if (N < (int)pq.size()) pq.pop();
-  }
-  cout << pq.top() << '\n';
+    priority_queue<int, vector<int>, greater<>> pq; // 최소힙
+    int N;
+    cin >> N;
+    for (int i=0; i<N*N; i++){
+        int tmp;
+        cin >> tmp;
+        pq.push(tmp);
+        if (pq.size() > N) pq.pop(); // 상위 5개만 큐에 남겨놓기
+    }
+
+    cout << pq.top();
+
+    return 0;
 }
-
-/*
-문제의 메모리 제한을 고려해 우선순위 큐에 상위 N개의 값만 가지고 있는다.
-*/
