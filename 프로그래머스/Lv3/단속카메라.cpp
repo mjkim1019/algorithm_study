9590ca124854f8eb200a3a938a3f07f2348b5879
Minju Kim <50831854+mjkim1019@users.noreply.github.com>
2023-08-10 14:47:11 +0900

    끝점이 시작점이 되도록 (끝점 작은 순으로 정렬)
    끝점보다 시작점이 크면 새로운 감시카메라 필요

--- 프로그래머스/Lv3/단속카메라.cpp
+++ 프로그래머스/Lv3/단속카메라.cpp
@@ -4,23 +4,21 @@ using namespace std;
 // 구하는 것 : 모든 차량이 카메라 만나도록 최소 몇 대의 카메라를 설치해야 하는지
 // 해결법 : 최대한 겹치는대로
 
+bool cmp(vector<int> &a, vector<int> &b){
+    return a[1] < b[1]; // 빨리 나가는 순서대로 정렬
+}
+
 int solution(vector<vector<int>> routes) {
     int answer = 0;
-    sort(routes.begin(), routes.end());
+    sort(routes.begin(), routes.end(), cmp);
+    for (auto v: routes) cout << v[0] << ", " << v[1] <<'\n';
    
-    int st = routes[0][0];
-    int en = routes[0][1];
-    answer++;
-    for (int i=1; i<routes.size(); i++){
-        if (en < routes[i][0] || st > routes[i][1]) { // 범위 안겹침
+    int limit = -30001;
+    for (int i=0; i<routes.size(); i++){
+        if (limit < routes[i][0]){ // 진입점이 나가는 지점보다 크면 감시카메라 필요
             answer++;
-            st = routes[i][0];
-            en = routes[i][1];
-            continue;
+            limit = routes[i][1];
         }
-        // 범위 겹침
-        st = max(st, routes[i][0]);
-        en = min(en, routes[i][1]);
     }
     
     return answer;
